2de69eb6770498eca80f26b477497e5d33099b3f
Andrew Levin <andrewlevingit@hotmail.com>
2024-11-26 18:59:32 +0300

    2024-11-26 18:59:32 N

---
+++ src/cozyspots-config-default-v2.0.js
@@ -0,0 +1,10 @@
+
+let config = {
+    'rootURL': '',
+    'map': {
+        'center':  [37.61, 55.75],
+        'zoom': 11,
+        'style': 'mapbox://styles/mapbox/outdoors-v12'
+    },
+    'data_path': 'data.yaml'
+};
