007a7a4c573d4e5d5ffc668d39ba3ce056286f90
Andrew Levin <andrewlevingit@hotmail.com>
2025-01-01 04:36:16 +0300

    2025-01-01 04:36:16 N

--- src/cozyspots-v2.5.js
+++ src/cozyspots-v2.5.js
@@ -14,7 +14,7 @@ function radiusLine(zoom) {
 }
 
 function getRadius(zoom) {
-    const radius = Math.max(5 * (zoom - 9) + 15, 12);
+    const radius = Math.max(5 * (zoom - 9) + 15, 15);
     console.log('zoom:', zoom, 'radius: ', radius);
     return Math.round(radius + 2);
 }
